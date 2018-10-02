df4240ecf916344c7b191591b277c6f9120e566e
WJack611 <43244713+WJack611@users.noreply.github.com>
2018-10-02 23:17:15 +0800

    Update Menu3.cpp

--- w04/Menu3.cpp
+++ w04/Menu3.cpp
@@ -5,79 +5,234 @@ void prchar(char c, int n)
         printf("%c", c);
 }
 void drawRect(int height, int width, int filled)
-{if(filled){
-    for(int i=1; i<=height; i++)
+{
+    if(filled)
     {
-        prchar('*', width);
-        printf("\n");
+        for(int i=1; i<=height; i++)
+        {
+            prchar('*', width);
+            printf("\n");
+        }
     }
-}else{ for(int i=1; i<=height; i++)
+    else
     {
-        prchar(' ',width-2);
-        prchar('*',width);
-        printf("\n");
+        for(int i=1; i<=height; i++)
+        {
+            if(i==1||i==height)
+            {
+                prchar('*',width);
+                printf("\n");
+            }
+            else
+            {
+                prchar('*',1);
+                prchar(' ',width-2);
+                prchar('*',1);
+                printf("\n");
+            }
+
+        }
     }
 }
-}
-void drawVertTran(int height, int type, int filled)
+void drawVertTrian(int height, int type, int filled)
 {
     switch(type)
     {
     case 1:
-        for(int i=1; i<=height; i++)
+        if(filled)
         {
-            for(int j=1; j<=height; j++)
+            for(int i=1; i<=height; i++)
             {
-                if(j==1||i==j||i==height)
-                    printf("*");
-                else
-                    printf(" ");
+                prchar('*',i);
+                printf("\n");
             }
-            printf("\n");
         }
+        else
+            for(int i=1; i<=height; i++)
+            {
+                if(i==1||i==height)
+                {
+                    prchar('*',i);
+                    printf("\n");
+                }
+                else
+                {
+                    prchar('*',1);
+                    prchar(' ',i-2);
+                    prchar('*',1);
+                    printf("\n");
+                }
+            }
+
         break;
     case 2:
-        for(int i=1; i<=height; i++)
+        if(filled)
         {
-            for(int j=1; j<=height; j++)
+            for(int i=1; i<=height; i++)
             {
-                if(i==height||j==height||i+j==(height+1))
-                    printf("*");
-                else
-                    printf(" ");
+                prchar('*',height-i+1);
+                printf("\n");
             }
-            printf("\n");
+        }
+        else
+        {
+            for(int i=1; i<=height; i++)
+            {
+                if(i==1||i==height)
+                {
+                    prchar('*',height-i+1);
+                    printf("\n");
+                }
+                else
+                {
+                    prchar('*',1);
+                    prchar(' ',height-i-1);
+                    prchar('*',1);
+                    printf("\n");
+                }
+            }
         }
         break;
     case 3:
-        for(int i=1; i<=height; i++)
+        if(filled)
         {
-            for(int j=1; j<=height; j++)
+            for(int i=1; i<=height; i++)
             {
-                if(i==1||j==1||i+j==(height+1))
-                    printf("*");
-                else
-                    printf(" ");
+                prchar(' ',i-1);
+                prchar('*',height-i+1);
+                printf("\n");
             }
-            printf("\n");
         }
+        else
+        {
+            for(int i=1; i<=height; i++)
+            {
+                if(i==1||i==height)
+                {
+                    prchar(' ',i-1);
+                    prchar('*',height-i+1);
+                    printf("\n");
+                }
+
+
+                else
+                {
+                    prchar(' ',i-1);
+                    prchar('*',1);
+                    prchar(' ',height-i-1);
+                    prchar('*',1);
+                    printf("\n");
+                }
+            }
+        }
+
         break;
     case 4:
-        for(int i=1; i<=height; i++)
+        if(filled)
         {
-            for(int j=1; j<=height; j++)
+            for(int i=1; i<=height; i++)
             {
-                if(i==1||j==height||i==j)
-                    printf("*");
-                else
-                    printf(" ");
+                prchar(' ',height-i);
+                prchar('*',i);
+                printf("\n");
             }
-            printf("\n");
         }
+        else
+        {
+            for(int i=1; i<=height; i++)
+            {
+                if(i==1||i==height)
+                {
+                    prchar(' ',height-i);
+                    prchar('*',i);
+                    printf("\n");
+                }
+                else
+                {
+                    prchar(' ',height-i);
+                    prchar('*',1);
+                    prchar(' ',i-2);
+                    prchar('*',1);
+                    printf("\n");
+                }
+            }
+
+        }
+
         break;
 
     }
 
+}
+void drawIsosTrian(int height, int type, int filled)
+{
+    switch(type)
+    {
+    case 1:
+        if(filled)
+        {
+            for(int i=1; i<=height; i++)
+            {
+                prchar(' ',height-i+1);
+                prchar('*',i*2-1);
+                prchar(' ',height-i+1);
+                printf("\n");
+            }
+        }
+        else
+        {
+            for(int i=1; i<=height; i++)
+            {
+                if(i==1||i==height)
+                {
+
+                    prchar(' ',height-i+1);
+                    prchar('*',i*2-1);
+                    printf("\n");
+                }
+                else
+                {
+                    prchar(' ',height-i+1);
+                    prchar('*',1);
+                    prchar(' ',i*2-3);
+                    prchar('*',1);
+                    printf("\n");
+                }
+            }
+        }
+        break;
+    case 2:
+        if(filled)
+        {
+            for(int i=1; i<=height; i++)
+            {
+                prchar(' ',i-1);
+                prchar('*',(height-i+1)*2-1);
+                printf("\n");
+            }
+        }
+        else
+        {
+            for(int i=1; i<=height; i++)
+            {
+                if(i==1||i==height)
+                {
+                    prchar(' ',i-1);
+                    prchar('*',(height-i+1)*2-1);
+                    printf("\n");
+                }
+                else
+                {
+                    prchar(' ',i-1);
+                    prchar('*',1);
+                    prchar(' ',(height-i+1)*2-3);
+                    prchar('*',1);
+                    printf("\n");
+                }
+            }
+        }
+        break;
+    }
 }
 int main()
 {
@@ -90,11 +245,12 @@ int main()
     {
         printf("Main Menu\n");
         printf("1. Rectangle\n");
-        printf("2. Vertical Trangle\n");
-        printf("3. Exit\n");
+        printf("2. Vertical Triangle\n");
+        printf("3. Isosceles Triangle\n");
+        printf("4. Exit\n");
         printf("=>");
         scanf("%d",&choice);
-        if(choice==3)
+        if(choice==4)
             break;
         switch(choice)
         {
@@ -106,13 +262,18 @@ int main()
         case 2:
             printf("Enter height type filled:");
             scanf("%d %d %d", &height, &type, &filled);
-            drawVertTran(height,type,filled);
+            drawVertTrian(height,type,filled);
             break;
+        case 3:
+            printf("Enter height type filled:");
+            scanf("%d %d %d", &height, &type, &filled);
+            drawIsosTrian(height,type,filled);
         }
     }
     return 0;
 
 }
+
 
 
 
